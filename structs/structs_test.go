12f7a8ba15c96b3dda90be0f203e35f409c3a038
tricej <54190313+tricej@users.noreply.github.com>
2024-03-19 02:40:42 +0000

    Added more

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -27,24 +27,22 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	t.Run("rectangles", func(t *testing.T) {
-		r := Rectangle{12, 6}
-		got := r.Area()
-		want := 72.0
-
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72.0)
 	})
 
 	t.Run("circles", func(t *testing.T) {
-		c := Circle{10}
-		got := c.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
 	})
 
 }
